395f4a70058333d103bd0be883f94ef84c4374b4
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2026-02-03 17:43:19 +0630

    chore: add indieship logo

---
+++ src/components/built-with-indie-ship.tsx
@@ -0,0 +1,11 @@
+export const BuiltWithIndieShip = () => {
+  return (
+    <a
+      href="https://indieship.netlify.app"
+      className="font-bold rounded text-sm px-2 py-1 text-primary flex items-center gap-2 w-fit"
+    >
+      Built with{" "}
+      <img src="/indieship-logo.png" className="w-16" alt="IndieShip Logo" />
+    </a>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { BuiltWithIndieShip } from "~/components/built-with-indie-ship";
+
 export const Route = createFileRoute("/")({
   component: App
 });
 
 function App() {
-  return <p>This is index page</p>;
+  return (
+    <p>
+      This is index page <BuiltWithIndieShip />
+    </p>
+  );
 }
