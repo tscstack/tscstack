47b93cd136c4dce0034d413ff3fdfc86960261b9
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2026-02-03 14:43:27 +0630

    chore: add auth function

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
+import { requireAuthFn } from "~/functions/auth";
+
 export const Route = createFileRoute("/_authenticated")({
+  beforeLoad: async ({ location }) => {
+    return await requireAuthFn({ data: { href: location.href } });
+  },
   component: RouteComponent
 });
 
